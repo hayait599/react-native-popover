31826f960e3ad6809a3f7480fac2ab8d5f7414d5
HaYa Abu Al Halawa <h.hayas123@gmail.com>
2018-01-07 11:13:06 +0200

    Update Popover.js

--- Popover.js
+++ Popover.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'PropTypes';
+
 import {StyleSheet, Dimensions, Animated, Text, TouchableWithoutFeedback, View} from 'react-native';
 import _ from 'underscore';
 
